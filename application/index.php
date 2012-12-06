136b960e721862b9b62e07a1bc31b465ff585139
Renee Marie <renee.blunt@gmail.com>
2012-12-06 16:28:56 -0500

    added controllers

--- application/index.php
+++ application/index.php
@@ -1,4 +1,14 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
-	include('views/login.html');
+	include_once('models/controllerModel.php');
+	$controllerModel = new controllerModel();	
+	if(empty($_GET['controller'])){
+		$controllerModel->getController('map');
+	}else{
+		$controller = $_GET["controller"];
+		if($controller == "map"){
+			$controllerModel->getController('map');
+		}elseif($controller == "admin"){
+			$controllerModel->getController('admin');
+		}
+	}	
 ?>
