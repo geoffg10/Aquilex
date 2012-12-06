f05016103a1b9aff0914d70502fa76f2e2385af0
Renee Marie <renee.blunt@gmail.com>
2012-12-06 06:58:37 -0500

    created application folder for production
    please use production folder when working with features that coorespond
    with designed html
    use the demo folder for features that do not yet have the html built

---
+++ application/xhr/login.php
@@ -0,0 +1,14 @@
+<?php
+	require_once('../models/loginModel.php');
+	
+	$loginModel = new loginModel();
+	$message = array();
+	
+	if($_POST['email']!='' && $_POST['password']!=''){
+		$loginResult = $loginModel->checkUser($_POST);
+		echo json_encode(array('message'=>'connected', 'result'=>$loginResult));
+	}else{
+		$checkEmailResult = $loginModel->checkEmail($_POST);
+		echo json_encode(array('message'=>'something is missing', 'result'=>$checkEmailResult));
+	}
+?>
