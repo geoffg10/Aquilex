d3805fb24e3cb9454e1b14845342cb7a1283ef49
Renee Marie <renee.blunt@gmail.com>
2012-12-07 18:54:34 -0500

    added getrooms

--- application/models/locationsModel.php
+++ application/models/locationsModel.php
@@ -41,5 +41,21 @@
 				return 'no buildings found for this campus, be sure to name your variable campus_id';
 			}
 		}
+		public function getRooms($data){
+			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
+			$sqlst = "SELECT r.id, r.building_id, r.latitude, r.longitude, r.name, rt.type
+						FROM rooms r
+						LEFT JOIN room_types rt on (r.room_type_id = rt.id)
+						WHERE building_id = :building_id";
+			$st = $db->prepare($sqlst);
+			$results = $st->execute(array(":building_id"=>$data['building_id']));
+			$resultData = $st->fetchAll(); //get all responses
+			if($st->rowCount() > 0){ //if the record exists than 
+				//there is a record
+				return $resultData;
+			}else{
+				return 'no rooms found for this building, be sure to name your variable building_id';
+			}
+		}
 	}
 ?>
