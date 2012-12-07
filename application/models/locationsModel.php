2a425df968d47e97bfca0f9a25aaf0ab6903d2da
Renee Marie <renee.blunt@gmail.com>
2012-12-07 18:17:13 -0500

    added get locations call

--- application/models/locationsModel.php
+++ application/models/locationsModel.php
@@ -7,5 +7,15 @@
 			$st = $db->prepare($sqlst);
 			$results = $st->execute(array(":longitude"=>$data['longitude'], ":latitude"=>$data['latitude']));
 		} //close add location
+		
+		public function getLocation($data){
+			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
+			$sqlst = "SELECT c.id, c.name, c.latitude, c.longitude, c.google_ref_id, ct.type
+						FROM campuses c
+						LEFT JOIN campus_types ct on(c.campus_type_id = ct.id)
+						where google_ref_id = :google_ref_id";
+			$st = $db->prepare($sqlst);
+			$results = $st->execute(array(":google_ref_id"=>$data['google_ref_id']));
+		}
 	}
 ?>
---
+++ application/xhr/getlocations.php
@@ -0,0 +1,12 @@
+<?php
+	require_once('../models/locationsModel.php');
+	
+	$locationsModel = new locationsModel();
+	$message = array();
+	
+	if($_POST){
+		$locations = $locationsModel->getLocation($_POST);
+		echo json_encode(array('message'=>'locations', 'result'=>$locations))
+	}
+
+?>
