b9bc0f5d9c5a38962502808214731fa6f1ac96f8
Renee Marie <renee.blunt@gmail.com>
2012-12-07 19:40:23 -0500

    udpate locations return

--- application/models/locationsModel.php
+++ application/models/locationsModel.php
@@ -8,16 +8,20 @@
 			$results = $st->execute(array(":longitude"=>$data['longitude'], ":latitude"=>$data['latitude']));
 		} //close add location
 		
+		
+		//returns the 
 		public function addCampus($data){  
 			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
 			$sqlst = "insert into locations(name, longitude, latitude, campus_type_id, added_by_id, google_ref_id)values(:name, :longitude, :latitude, :campus_type_id, :added_by_id)";
 			$st = $db->prepare($sqlst);
-			$results = $st->execute(array(":name"=>$data['name'],":longitude"=>$data['longitude'], ":latitude"=>$data['latitude'], ":campus_type_id"=>'1', ":added_by_id"=>$data['added_by_id']));
-		} //close add location
+			$results = $st->execute(array(":name"=>$data['name'],":longitude"=>$data['longitude'], ":latitude"=>$data['latitude'], ":campus_type_id"=>'1', ":added_by_id"=>$data['added_by_id'], ":google_ref_id"=>$data['google_ref_id']));
+			$getLocationData = $this->getLocation($data['google_ref_id']); //getting location data
+			return $getLocationData;
+		} //close addCampus
 		
 		// gets location by the google_ref_id
 		//returns the object with it's id, name, lat, long, google_ref_id, type
-		public function getLocation($data){ //expecting 
+		public function getLocation($data, $internal=FALSE){ //expecting 
 			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
 			$sqlst = "SELECT c.id, c.name, c.latitude, c.longitude, c.google_ref_id, ct.type
 						FROM campuses c
@@ -30,9 +34,10 @@
 				//there is a record
 				return $resultData;
 			}else{
-				return 'no record found, double check to make sure you name the variable google_ref_id';
+				return 'no record';
 			}
-		}
+		}//close get location
+		
 		// gets building by the campus_id
 		//returns the object with it's id, name, lat, long, campus_id
 		public function getBuildings($data){
@@ -47,10 +52,11 @@
 				//there is a record
 				return $resultData;
 			}else{
-				return 'no buildings found for this campus, be sure to name your variable campus_id';
+				return 'no record';
 			}
-		}
-		// gets location by the building_id
+		}//close get buildings
+		
+		// gets rooms by the building_id
 		//returns the object with it's id, name, lat, long, building_id, type
 		public function getRooms($data){
 			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
@@ -65,8 +71,8 @@
 				//there is a record
 				return $resultData;
 			}else{
-				return 'no rooms found for this building, be sure to name your variable building_id';
+				return 'no record';
 			}
-		}
+		} //close get rooms
 	}
 ?>
