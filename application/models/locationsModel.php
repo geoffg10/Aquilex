c95e317566010587000f6b648143a91d76be8a2e
Renee Marie <renee.blunt@gmail.com>
2012-12-08 14:32:24 -0500

    update locations model to fix add campus

--- application/models/locationsModel.php
+++ application/models/locationsModel.php
@@ -12,7 +12,7 @@
 		//returns the 
 		private function addCampus($data){  
 			$db = new \PDO("mysql:hostname=127.0.0.1;port=8889;dbname=aquilex", "root", "root");
-			$sqlst = "insert into locations(name, longitude, latitude, campus_type_id, added_by_id, google_ref_id)values(:name, :longitude, :latitude, :campus_type_id, :added_by_id)";
+			$sqlst = "insert into campuses(name, longitude, latitude, campus_type_id, added_by_id, google_ref_id)values(:name, :longitude, :latitude, :campus_type_id, :added_by_id, :google_ref_id)";
 			$st = $db->prepare($sqlst);
 			$results = $st->execute(array(":name"=>$data['name'],":longitude"=>$data['longitude'], ":latitude"=>$data['latitude'], ":campus_type_id"=>'1', ":added_by_id"=>$data['added_by_id'], ":google_ref_id"=>$data['google_ref_id']));
 			$getLocationData = $this->getLocation($data['google_ref_id']); //getting location data
