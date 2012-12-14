741b178c8aae158527725cdb1902a2ae92b511eb
Geoffrey <gf4re@bellsouth.net>
2012-12-14 00:09:43 -0500

    styled the map style
    schemed style

--- application/js/main.js
+++ application/js/main.js
@@ -90,6 +90,11 @@ $(document).ready(function () {
             icon:image
 
         });
+        
+        
+        
+        
+        
         infowindow = new google.maps.InfoWindow(); //create infowindow, this is called later to display the location details when clicked
 
         google.maps.event.addListener(map, 'zoom_changed', function () {
@@ -113,6 +118,41 @@ $(document).ready(function () {
             console.log('draggy', userMarker.getPosition());
 
         });
+        
+        
+        
+        
+         var styles = [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
 
         /*===================================================================================================== ##2 getUniversities runs after initialize on load
          */
